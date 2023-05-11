87d48ecb14f28dc69ce8a7de1c28a2ed51605988
hoteaklee <cc18ht@naver.com>
2023-05-11 17:41:04 +0900

    Initial commit

---
+++ src/main/java/zzyzzy/springboot/semiprojectv5/Semiprojectv5bApplication.java
@@ -0,0 +1,13 @@
+package zzyzzy.springboot.semiprojectv5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Semiprojectv5bApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Semiprojectv5bApplication.class, args);
+    }
+
+}
---
+++ src/test/java/zzyzzy/springboot/semiprojectv5/Semiprojectv5bApplicationTests.java
@@ -0,0 +1,13 @@
+package zzyzzy.springboot.semiprojectv5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Semiprojectv5bApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
